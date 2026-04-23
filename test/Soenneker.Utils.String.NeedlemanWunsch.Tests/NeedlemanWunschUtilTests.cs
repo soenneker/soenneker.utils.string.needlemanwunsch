5fcbc142e0fefd67947a2e513f78a04818ffc23d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:05 -0500

    Various fixes

--- test/Soenneker.Utils.String.NeedlemanWunsch.Tests/NeedlemanWunschUtilTests.cs
+++ test/Soenneker.Utils.String.NeedlemanWunsch.Tests/NeedlemanWunschUtilTests.cs
@@ -11,15 +11,15 @@ public class NeedlemanWunschUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("", "", 0)]
-    [InlineData("abc", "", 3)]
-    [InlineData("", "xyz", 3)]
-    [InlineData("kitten", "sitting", 3)]
-    [InlineData("kitten", "kitten", 0)]
-    [InlineData("abc", "def", 3)]
-    [InlineData("abcdef", "abc", 3)]
-    [InlineData("abc", "abcd", 1)]
+    [Test]
+    [Arguments("", "", 0)]
+    [Arguments("abc", "", 3)]
+    [Arguments("", "xyz", 3)]
+    [Arguments("kitten", "sitting", 3)]
+    [Arguments("kitten", "kitten", 0)]
+    [Arguments("abc", "def", 3)]
+    [Arguments("abcdef", "abc", 3)]
+    [Arguments("abc", "abcd", 1)]
     public void CalculateSimilarity_Returns_Correct_Similarity_Score(string str1, string str2, int expectedScore)
     {
         int similarityScore = NeedlemanWunschStringUtil.CalculateSimilarity(str1, str2);
@@ -28,15 +28,15 @@ public class NeedlemanWunschUtilTests : HostedUnitTest
     }
 
 
-    [Theory]
-    [InlineData("", "", 100.0)]
-    [InlineData("abc", "", 0.0)]
-    [InlineData("", "xyz", 0.0)]
-    [InlineData("kitten", "sitting", 57.1428)]
-    [InlineData("kitten", "kitten", 100.0)]
-    [InlineData("abc", "def", 0.0)]
-    [InlineData("abcdef", "abc", 50.0)]
-    [InlineData("abc", "abcd", 75.0)]
+    [Test]
+    [Arguments("", "", 100.0)]
+    [Arguments("abc", "", 0.0)]
+    [Arguments("", "xyz", 0.0)]
+    [Arguments("kitten", "sitting", 57.1428)]
+    [Arguments("kitten", "kitten", 100.0)]
+    [Arguments("abc", "def", 0.0)]
+    [Arguments("abcdef", "abc", 50.0)]
+    [Arguments("abc", "abcd", 75.0)]
     public void CalculateSimilarityPercentage_Returns_Correct_Percentage(string str1, string str2, double expectedPercentage)
     {
         double similarityPercentage = NeedlemanWunschStringUtil.CalculateSimilarityPercentage(str1, str2);
@@ -44,3 +44,4 @@ public class NeedlemanWunschUtilTests : HostedUnitTest
         similarityPercentage.Should().BeApproximately(expectedPercentage, 0.001);
     }
 }
+
